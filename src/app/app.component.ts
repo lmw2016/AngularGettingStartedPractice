9216d111d9ab22be0fd8fc93387ce811d0cf89f5
liming wu <limwuly@gmail.com>
2019-07-05 15:50:13 -0400

    first change with AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'pm-root',
+  template:`
+    <div><h2>{{pageTitle}}</h2>
+    <div>my first component == andrew </div>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular: Getting Started --Oh yeah!!';
+export class AppComponent{
+  pageTitle:string='Acme product Management';
 }
