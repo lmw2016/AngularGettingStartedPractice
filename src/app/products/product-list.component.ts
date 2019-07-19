ccf4927c9b2e11839c3a6517a97f6a64711d481d
liming wu <limwuly@gmail.com>
2019-07-19 16:28:55 -0400

    add project service DI

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, OnChanges, OnDestroy } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
    selector:'pm-products',
    templateUrl:'./product-list.component.html',
    styleUrls:['./product-list.component.css']
+   //providers:[ProductService]
 })
 export class ProductListComponent implements OnInit, OnChanges,OnDestroy{
 
@@ -29,45 +31,13 @@ export class ProductListComponent implements OnInit, OnChanges,OnDestroy{
 
 
     filteredProducts:IProduct[];
-    products:IProduct[]=[
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-          },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          }
-    ]
+    products:IProduct[]=[]
+      
 
     /**
      *
      */
-    constructor() {
-      this.filteredProducts=this.products;
-      this.listFilter='cart';
+    constructor(private productService: ProductService) {
       
     }
 
@@ -83,6 +53,8 @@ export class ProductListComponent implements OnInit, OnChanges,OnDestroy{
 
     ngOnInit(): void {
       console.log('onInit');
+      this.products=this.productService.getProducts();
+      this.filteredProducts=this.products;
     }
 
     ngOnDestroy(): void {
