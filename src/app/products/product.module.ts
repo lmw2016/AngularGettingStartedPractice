b59c72fc7de099b7459c30379d2b68fcc4088f01
liming wu <limwuly@gmail.com>
2019-08-03 14:37:40 -0400

    implement sharedmodule: export start component,commommodule and formsModule

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -2,28 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { ConvertToSpacesPipe } from '../shared/convert.to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+import { ConvertToSpacesPipe } from '../shared/convert.to-spaces.pipe';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path:'products',component:ProductListComponent},
       {path:'products/:id',
         canActivate:[ProductDetailGuard],
         component:ProductDetailComponent},
-    ])
+    ]),
+    SharedModule
   ],
 
 })
